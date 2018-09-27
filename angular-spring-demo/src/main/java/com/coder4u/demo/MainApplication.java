828161fe1df65c18ebfef4606a040c55cfc335e5
Sumesh TG <mailbox4sumesh@gmail.com>
2018-09-27 16:16:53 +0530

    package renamed

---
+++ angular-spring-demo/src/main/java/com/coder4u/demo/MainApplication.java
@@ -0,0 +1,25 @@
+package com.coder4u.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+
+//@EnableAutoConfiguration
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class, UserDetailsServiceAutoConfiguration.class })
+@ComponentScan("com.coder4u")
+public class MainApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(MainApplication.class);
+	}
+}
---
+++ angular-spring-demo/src/main/java/com/coder4u/home/HomeController.java
@@ -0,0 +1,22 @@
+package com.coder4u.home;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	@RequestMapping(value = "test", method = RequestMethod.GET)
+    public Object testRest(){
+		Map<String,String> mapTest=new HashMap<>();
+		mapTest.put("1", "data1");
+		mapTest.put("2", "data2");
+		mapTest.put("3", "data3");
+		mapTest.put("4", "data4");
+		mapTest.put("5", "data5");
+		return mapTest;
+	}
+}
